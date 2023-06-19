22b30c785686eaffe24ac398ed7c249ef48edfdf
Nicolas Franck <nicolas.franck@ugent.be>
2023-06-19 11:57:13 +0200

    remove unused method Person#Dup and Organization#Dup

--- models/organization.go
+++ models/organization.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	v1 "github.com/ugent-library/people/api/v1"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Organization struct {
@@ -12,33 +11,4 @@ type Organization struct {
 
 func (org *Organization) IsStored() bool {
 	return org.DateCreated != nil
-}
-
-func (org *Organization) Dup() *Organization {
-	// *org copies mutex values too..
-	no := &Organization{
-		Organization: &v1.Organization{
-			Id:       org.Id,
-			Type:     org.Type,
-			NameDut:  org.NameDut,
-			NameEng:  org.NameEng,
-			ParentId: org.ParentId,
-		},
-	}
-
-	if org.DateCreated != nil {
-		no.DateCreated = timestamppb.New(org.DateCreated.AsTime())
-	}
-	if org.DateUpdated != nil {
-		no.DateUpdated = timestamppb.New(org.DateUpdated.AsTime())
-	}
-	no.OtherId = make([]*v1.IdRef, 0, len(org.OtherId))
-	for _, oldIdRef := range org.OtherId {
-		no.OtherId = append(no.OtherId, &v1.IdRef{
-			Id:   oldIdRef.Id,
-			Type: oldIdRef.Type,
-		})
-	}
-
-	return no
 }
--- models/person.go
+++ models/person.go
@@ -2,7 +2,6 @@ package models
 
 import (
 	v1 "github.com/ugent-library/people/api/v1"
-	"google.golang.org/protobuf/types/known/timestamppb"
 )
 
 type Person struct {
@@ -13,42 +12,4 @@ type Person struct {
 
 func (person *Person) IsStored() bool {
 	return person.DateCreated != nil
-}
-
-func (p *Person) Dup() *Person {
-	// *p copies mutex values too..
-	np := &Person{
-		Person: &v1.Person{
-			Id:                 p.Id,
-			Active:             p.Active,
-			FullName:           p.FullName,
-			FirstName:          p.FirstName,
-			LastName:           p.LastName,
-			Email:              p.Email,
-			Orcid:              p.Orcid,
-			OrcidToken:         p.OrcidToken,
-			JobCategory:        append([]string{}, p.JobCategory...),
-			OrganizationId:     append([]string{}, p.OrganizationId...),
-			PreferredFirstName: p.PreferredFirstName,
-			PreferredLastName:  p.PreferredLastName,
-			BirthDate:          p.BirthDate,
-			Title:              p.Title,
-		},
-	}
-
-	if p.DateCreated != nil {
-		np.DateCreated = timestamppb.New(p.DateCreated.AsTime())
-	}
-	if p.DateUpdated != nil {
-		np.DateUpdated = timestamppb.New(p.DateUpdated.AsTime())
-	}
-	np.OtherId = make([]*v1.IdRef, 0, len(p.OtherId))
-	for _, oldIdRef := range p.OtherId {
-		np.OtherId = append(np.OtherId, &v1.IdRef{
-			Id:   oldIdRef.Id,
-			Type: oldIdRef.Type,
-		})
-	}
-
-	return np
 }

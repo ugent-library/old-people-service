49e7d8ecd7c1c9cf50992aa76fe2b92b26f71ba2
Nicolas Franck <nicolas.franck@ugent.be>
2023-07-05 14:27:38 +0200

    add ldap configuration

--- cmd/config.go
+++ cmd/config.go
@@ -20,11 +20,18 @@ type ConfigApi struct {
 	Password string `json:"password,omitempty" env:"PASSWORD,notEmpty"`
 }
 
+type ConfigLdap struct {
+	Url      string `json:"url,omitempty" env:"URL,notEmpty"`
+	Username string `json:"username,omitempty" env:"USERNAME,notEmpty"`
+	Password string `json:"password,omitempty" env:"PASSWORD,notEmpty"`
+}
+
 type Config struct {
 	Production bool       `json:"production" env:"PRODUCTION"`
 	Db         ConfigDb   `json:"db,omitempty" envPrefix:"DB_"`
 	Nats       ConfigNats `json:"nats,omitempty" envPrefix:"NATS_"`
 	Api        ConfigApi  `json:"api,omitempty" envPrefix:"API_"`
+	Ldap       ConfigLdap `json:"ldap,omitempty" envPrefix:"LDAP_"`
 }
 
 func (ca ConfigApi) Addr() string {

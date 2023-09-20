5b904681f93710004fe8e3431e74f0b18e63c791
Nicolas Franck <nicolas.franck@ugent.be>
2023-09-20 15:48:09 +0200

    use cobra.CheckErr to handle error root command

--- cli/cli.go
+++ cli/cli.go
@@ -1,9 +1,6 @@
 package cli
 
 import (
-	"fmt"
-	"os"
-
 	"github.com/caarlos0/env/v8"
 	"github.com/spf13/cobra"
 	"github.com/ugent-library/people-service/models"
@@ -66,8 +63,5 @@ func newUgentLdapClient() *ugentldap.Client {
 }
 
 func Execute() {
-	if err := rootCmd.Execute(); err != nil {
-		fmt.Fprintf(os.Stderr, "Whoops. There was an error while executing your CLI '%s'", err)
-		os.Exit(1)
-	}
+	cobra.CheckErr(rootCmd.Execute())
 }
